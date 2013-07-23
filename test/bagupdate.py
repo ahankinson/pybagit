0d63b13bcc8253749f4ea97099dbba322b6c3790
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2013-07-23 14:03:05 -0400

    New: unit tests for testing is_valid method.

--- test/bagupdate.py
+++ test/bagupdate.py
@@ -1,19 +1,33 @@
 import unittest
 import os
+import shutil
 from pybagit.bagit import BagIt
 
+
 class UpdateTest(unittest.TestCase):
-    
+
     def setUp(self):
         print "Setting up update."
         self.bag = BagIt(os.path.join(os.getcwd(), 'test', 'testbag'))
-    
+        self.invalid_bag = BagIt(os.path.join(os.getcwd(), 'test', 'invalid_bag'))
+
     def tearDown(self):
-        pass
-    
+        if os.path.exists(os.path.join(os.getcwd(), 'test', 'invalid_bag')):
+            shutil.rmtree(os.path.join(os.getcwd(), 'test', 'invalid_bag'))
+
     def test_update(self):
         self.bag.update()
         self.assertEquals(len(self.bag.bag_errors), 0)
+
+    def test_is_valid(self):
+        self.bag.update()
+        self.assertEquals(self.bag.is_valid(), True)
+
+    def test_not_valid(self):
+        os.remove(self.invalid_bag.manifest_file)
+        self.invalid_bag.validate()
+        self.assertEquals(self.invalid_bag.is_valid(), False)
+
 
 def suite():
     test_suite = unittest.makeSuite(UpdateTest, 'test')
